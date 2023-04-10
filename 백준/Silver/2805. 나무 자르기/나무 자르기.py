cd87aba65c6c138248f606b3d2fa3552694ce373
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-04-11 01:12:43 +0900

    [Silver II] Title: 나무 자르기, Time: 1056 ms, Memory: 327884 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,12 +1,14 @@
 import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
-trees = list(map(int, sys.stdin.readline().split()))
+trees = list(map(int, input().split()))
 start, end = 1, max(trees)
-while end >= start:
-    mid = (end + start)//2
-    result = sum([tree-mid for tree in trees if tree > mid])
-    if result >= M:
-        start = mid + 1
+while start <= end:
+    mid = (start + end) // 2
+    sum_tree = sum([tree - mid for tree in trees if tree > mid])
+    if sum_tree >= M:
+        start = mid+1
     else:
-        end = mid - 1
+        end = mid-1
 print(end)
