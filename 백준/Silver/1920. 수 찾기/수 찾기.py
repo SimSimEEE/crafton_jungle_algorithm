d609b79a137e5443b68b3346b9affe8fc9853ad0
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-04-10 23:56:58 +0900

    [Silver IV] Title: 수 찾기, Time: 436 ms, Memory: 139280 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,8 +1,25 @@
-N = int(input())
-A = set(map(int,input().split()))
-M = int(input())
-for B in list(map(int,input().split())):
-    if B in A:
-        print(1)
+import sys
+input = sys.stdin.readline
+
+
+def BS(start, finish, A, B):
+    if start > finish:
+        return 0
+
+    mid = (finish+start)//2
+
+    if A[mid] == B:
+        return 1
+    elif A[mid] > B:
+        return BS(start, mid-1, A, B)
     else:
-        print(0)
+        return BS(mid+1, finish, A, B)
+
+
+N = int(input())
+A = sorted(map(int, input().split()))
+M = int(input())
+B = map(int, input().split())
+
+for b in B:
+    print(BS(0, N-1, A, b))
