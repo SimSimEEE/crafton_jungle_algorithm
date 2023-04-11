3d87e9261d7b6c81205253c1f0e3e56ad550a366
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-04-11 10:09:27 +0900

    [Silver IV] Title: 한수, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,12 +1,12 @@
 import sys
 input = sys.stdin.readline
-n= int(input())
-count = 0
-if(n<100):
-    print(n)
-else:
-    count = 99
-    for i in range(100,n+1):   
-         if(i//100+i%10==2*int(i//10%10)):
-            count+=1
-    print(count)
+N = int(input())
+result = 0
+if N < 100:
+    print(N)
+else :
+    result = 99
+    for i in range(100, N+1):
+        if i//100+i%10 == 2*(i//10%10):
+            result += 1
+    print(result)
