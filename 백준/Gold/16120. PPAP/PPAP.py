22786f4a8d18c28c668fed6edbdf7478d1a04620
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-04-20 11:24:40 +0900

    [Gold IV] Title: PPAP, Time: 232 ms, Memory: 164368 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.py
+++ 백준/Gold/16120. PPAP/PPAP.py
@@ -1,8 +1,8 @@
 str_ppap = input()
 stack = []
 ppap = ["P","P","A","P"]
-for i in range(len(str_ppap)):
-    stack.append(str_ppap[i])
+for char_ppap in str_ppap:
+    stack.append(char_ppap)
     if stack[-4:] == ppap:
         for _ in range(3):
             stack.pop()
