a3dd154bb07735015695ccc33ca1414d2152c3b9
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-04-23 23:15:55 +0900

    [Gold III] Title: 아침 산책, Time: 3216 ms, Memory: 64796 KB -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,30 +1,35 @@
 import sys
 input = sys.stdin.readline
 
-def DFS(V,start):
-    global flag, result
-    flag[start] = flag[start] | 1 << V
-    flag[V] = flag[V] | 1 << start
+
+def DFS(V, cnt):
+    global flag
+    flag = flag | 1 << V
     for i in tree[V]:
-        if flag[start] & 1 << i == 0:
-            if side[i-1] == '0':
-                DFS(i,start)
-            else:
-                result+=1
+        if flag & 1 << i == 0:
+            if side[i-1] == '1':
+                cnt += 1
+            else :
+                cnt = DFS(i, cnt)
+
+    return cnt
+
 
 V = int(input())
 side = input().rstrip()
 tree = [[]for _ in range(V+1)]
+result = 0
 
 for _ in range(V-1):
     u, v = map(int, input().split())
     tree[u].append(v)
     tree[v].append(u)
+    if side[u-1] == '1' and side[v-1] == '1':
+        result += 2
 
-result = 0
-flag = [0]*(V+1)
+flag = 0
 for i in range(1, V+1):
-    if side[i-1] == '1':
-        DFS(i,i)
-
+    if side[i-1] == '0' and flag & 1 << i == 0:
+        tmp = DFS(i, 0)
+        result = tmp*(tmp-1)
 print(result)
