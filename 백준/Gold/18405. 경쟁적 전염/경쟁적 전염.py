867b15ed1d914279f49caba401e90b747a87dcbe
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-04-27 18:15:15 +0900

    [Gold V] Title: 경쟁적 전염, Time: 200 ms, Memory: 119416 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -5,30 +5,32 @@ dcol = [0, 0, 1, -1]
 
 def bfs():
     while q:
-        time, row, col = q.popleft()
+        virus, time, row, col = q.popleft()
         if time == terget_s:
             break
         for i in range(4):
             nrow = row + drow[i]
             ncol = col + dcol[i]
             if 0 <= nrow < N and 0 <= ncol < N:
-                if grape[nrow][ncol] == 0:
-                    q.append([time+1, nrow, ncol])
-                    grape[nrow][ncol] = grape[row][col]
+                if graph[nrow][ncol] == 0:
+                    q.append([virus, time+1, nrow, ncol])
+                    graph[nrow][ncol] = graph[row][col]
 
 N, K = map(int,input().split())
 
-grape = [list(map(int,input().split())) for _ in range(N)]
+graph = [list(map(int,input().split())) for _ in range(N)]
 
 terget_s, terget_x, terget_y = map(int,input().split())
 
-q = deque([])
-for k in range(1,K+1):
-    for i in range(N):
-        for j in range(N):
-            if grape[i][j] == k:
-                q.append([0,i,j])
+q = []
+
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] != 0:
+            q.append([graph[i][j],0,i,j])
+
+q = deque(sorted(q))
 
 bfs()
 
-print(grape[terget_x-1][terget_y-1])
+print(graph[terget_x-1][terget_y-1])
