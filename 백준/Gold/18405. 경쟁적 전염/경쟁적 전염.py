0f0b69285e7420631e07e79cde9a53b0bce862b1
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-04-27 18:16:26 +0900

    [Gold V] Title: 경쟁적 전염, Time: 192 ms, Memory: 118768 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -4,6 +4,15 @@ drow = [-1, 1, 0, 0]
 dcol = [0, 0, 1, -1]
 
 def bfs():
+    q = []
+    
+    for i in range(N):
+        for j in range(N):
+            if graph[i][j] != 0:
+                q.append([graph[i][j],0,i,j])
+
+    q = deque(sorted(q))
+
     while q:
         virus, time, row, col = q.popleft()
         if time == terget_s:
@@ -21,15 +30,6 @@ N, K = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(N)]
 
 terget_s, terget_x, terget_y = map(int,input().split())
-
-q = []
-
-for i in range(N):
-    for j in range(N):
-        if graph[i][j] != 0:
-            q.append([graph[i][j],0,i,j])
-
-q = deque(sorted(q))
 
 bfs()
 
