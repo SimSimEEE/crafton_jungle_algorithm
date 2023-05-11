7348609381c37c06738acfb182e16def36ebb685
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-05-11 23:04:14 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.py
@@ -0,0 +1,8 @@
+n = int(input())
+scores = list(int(input()) for _ in range(n))
+cnt = 0
+for i in range(n-2,-1,-1):
+    if scores[i] >= scores[i+1]:
+        cnt += scores[i] - scores[i+1] + 1
+        scores[i] = scores[i+1] - 1
+print(cnt)
