27188e07a28dad40f48deecd24e976998ebf901f
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-05-12 14:39:56 +0900

    [Silver II] Title: 마인크래프트, Time: 128 ms, Memory: 34504 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,22 +1,25 @@
 import sys
-n, m, b = map(int, sys.stdin.readline().split())
-table = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-height = 0
-ans = sys.maxsize
-for i in range(257):
-    max = 0
-    min = 0
-    for j in range(n):
-        for k in range(m):
-            if table[j][k] < i:
-                min += (i - table[j][k])
-            else:
-                max += (table[j][k] - i)
-    inventory = max + b
-    if inventory < min:
-        continue
-    time = 2 * max + min
-    if time <= ans:
-        ans = time
-        height = i
-print(ans, height)
+from collections import Counter
+
+n, m, inven = map(int, sys.stdin.readline().split())
+ground = []
+for _ in range(n): ground += map(int, sys.stdin.readline().split())
+height, time = 0, sys.maxsize
+
+min_h = min(ground)
+max_h = max(ground)
+_sum = sum(ground)
+ground = dict(Counter(ground))
+
+for i in range(min_h, max_h + 1):
+    if _sum + inven >= i * n * m:
+        cur_time = 0
+        for key in ground:
+            if key > i:
+                cur_time += (key - i) * ground[key] * 2
+            elif key < i:
+                cur_time += (i - key) * ground[key]
+        if cur_time <= time:
+            time = cur_time
+            height = i
+print(time, height)
