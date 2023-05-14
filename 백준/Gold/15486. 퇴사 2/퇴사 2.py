6ee05505b894c3fd5cde89fb19a7d9988c00f3ea
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-05-15 00:44:01 +0900

    [Gold V] Title: 퇴사 2, Time: 1288 ms, Memory: 334144 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+consult = [0] + list(list(map(int,input().split())) for _ in range(n))
+dp = [0] * (n+1)
+for i in range(1,n+1):
+    dp[i] = max(dp[i],dp[i-1])
+    if i + consult[i][0] - 1 <= n :
+        dp[i + consult[i][0] - 1] = max(dp[i + consult[i][0] - 1], dp[i-1] + consult[i][1])
+    
+print(dp[n])
