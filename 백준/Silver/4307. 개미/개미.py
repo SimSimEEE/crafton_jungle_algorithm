eb2e63d1a57051916f01c8f4cbdc9b29e148b0de
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-05-15 01:07:52 +0900

    [Silver I] Title: 개미, Time: 224 ms, Memory: 124224 KB -BaekjoonHub

--- 백준/Silver/4307. 개미/개미.py
+++ 백준/Silver/4307. 개미/개미.py
@@ -5,6 +5,6 @@ for _ in range(int(input())):
     ants = list(int(input()) for _ in range(n))
     max_time, min_time = 0, 0
     for ant in ants:
-        max_time = max(max_time,max(ant, l - ant))
+        max_time = max(max_time, max(ant, l - ant))
         min_time = max(min_time, min(ant, l - ant))
     print(min_time, max_time, sep='\n')
