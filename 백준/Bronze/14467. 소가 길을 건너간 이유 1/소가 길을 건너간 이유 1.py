80907d7624ef1c2fc8bb9df7046462ffc55ab056
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-05-17 15:25:50 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
@@ -2,11 +2,9 @@ cows = [-1]*11
 cnt = 0
 for _ in range(int(input())):
     for cow_num, idx  in [map(int,input().split())]:
-        if cows[cow_num] == -1:
-            cows[cow_num] = idx
-        elif cows[cow_num] != idx:
-            cnt+=1
-            cows[cow_num] = idx
-        else:
-            cows[cow_num] = idx
+        if cows[cow_num] != -1:
+            if cows[cow_num] != idx:
+                cnt+=1
+        cows[cow_num] = idx
+
 print(cnt)
