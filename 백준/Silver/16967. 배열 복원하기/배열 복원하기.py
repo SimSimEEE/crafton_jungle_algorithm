bcdf7c32f9f8ce3c7acedf55d9b931f979a007be
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-05-22 12:43:37 +0900

    [Silver III] Title: 배열 복원하기, Time: 260 ms, Memory: 38160 KB -BaekjoonHub

---
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
@@ -0,0 +1,15 @@
+H, W, X, Y = map(int, input().split())
+B = [list(map(int, input().split())) for _ in range(H + X)]
+A = [[0] * W for _ in range(H)]
+
+for i in range(H + X):
+    for j in range(W + Y):
+        if X <= i < H and Y <= j < W:
+            A[i][j] = B[i][j] - A[i - X][j - Y]
+        elif 0 <= i < H and 0 <= j < W:
+            A[i][j] = B[i][j]
+        elif X <= i < H + X and Y <= j < W + Y:
+            A[i - X][j - Y] = B[i][j]
+
+for row in A:
+    print(*row)
