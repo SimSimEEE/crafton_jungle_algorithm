c8117474ae15747eb660af1e7dee75cd852c9356
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-07-06 01:03:08 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 392 ms, Memory: 153892 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,17 +1,22 @@
 n = int(input())
-graph = [['*']*n for _ in range(n)]
+graph = [['*'] * n for _ in range(n)]
+
 
 def delete_star(n, x, y):
     if n == 1:
         return
-    for i in range(x + n//3, x + 2*n//3):
-        for j in range(y + n//3, y + 2*n//3):
+
+    third = n // 3
+    for i in range(x + third, x + 2 * third):
+        for j in range(y + third, y + 2 * third):
             graph[i][j] = " "
+
     for i in range(3):
         for j in range(3):
             if i != 1 or j != 1:
-                delete_star(n//3, x + i * (n//3), y + j * (n//3))
+                delete_star(third, x + i * third, y + j * third)
+
 
 delete_star(n, 0, 0)
-for i in range(n):
-    print(*graph[i], sep="")
+for row in graph:
+    print(''.join(row))
