b7fe6cde83e3bf3d549cf13d27435f301cac0e23
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-09-07 23:37:25 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -2,13 +2,15 @@ def solution(bridge_length, weight, truck_weights):
     answer = 0
     bridge = [0] * bridge_length
     bridge_weights = 0
+    truck_weights.reverse()
     while truck_weights:
         answer += 1
         bridge_weights -= bridge.pop(0)
-        if bridge_weights + truck_weights[0] <= weight :
-            bridge.append(truck_weights.pop(0))
-            bridge_weights += bridge[-1]
-        else :
+        if bridge_weights + truck_weights[-1] <= weight:
+            truck = truck_weights.pop()
+            bridge.append(truck)
+            bridge_weights += truck
+        else:
             bridge.append(0)
 
-    return answer + len(bridge)
+    return answer + bridge_length
