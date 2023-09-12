6f48ec71ffddd25f41f039c275a544d18a93fc17
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-09-12 18:03:09 +0900

    [level 3] Title: 불량 사용자, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,26 @@
+from itertools import product
+
+def solution(user_id, banned_id):
+    answer = 0
+    banned_idArr = [[] for _ in range(len(banned_id))]
+    
+    for i, bid in enumerate(banned_id):
+        for uid in user_id:
+            correct = True
+            if len(uid) != len(bid):
+                correct = False
+            else:
+                for idx, _ in enumerate(bid):
+                    if bid[idx] != '*':
+                        if bid[idx] != uid[idx]:
+                            correct = False
+                            break
+                if correct:
+                    banned_idArr[i].append(uid)
+                    
+    unique_combinations = set()
+    
+    for combination in product(*banned_idArr):
+        if len(set(combination)) == len(combination):
+            unique_combinations.add(tuple(sorted(combination)))
+    return len(unique_combinations)
