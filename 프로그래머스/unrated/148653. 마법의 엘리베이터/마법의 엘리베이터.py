a626d4d43629a802a9bf6763019d8293d04651ff
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-09-15 20:25:09 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -0,0 +1,15 @@
+def solution(storey):
+    answer = 0
+    while storey > 0:
+        tmp = storey % 10
+        storey //= 10
+        if tmp > 5:
+            storey += 1
+            tmp = 10 - tmp
+        elif tmp < 5:
+            tmp = tmp
+        else:
+            if storey%10 >= 5:
+                storey+=1
+        answer += tmp
+    return answer
