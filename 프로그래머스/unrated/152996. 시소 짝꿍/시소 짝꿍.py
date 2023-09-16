32a5af281c3f3d8fce1662159547ef3c6dedf73f
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-09-17 08:07:55 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,11 +1,9 @@
 from collections import Counter
-
 def solution(weights):
     answer = 0
     weight_counter = Counter(weights)
     unique_weights = list(weight_counter.keys())
     ratios = [2, 3, 4]
-
     for i in range(len(unique_weights)):
         for k in range(len(ratios) - 1):
             for j in range(k + 1, len(ratios)):
@@ -13,7 +11,5 @@ def solution(weights):
                 if ratio in weight_counter:
                     print(unique_weights[i], ratio)
                     answer += weight_counter[unique_weights[i]] * weight_counter[ratio]
-
         answer += weight_counter[unique_weights[i]] * (weight_counter[unique_weights[i]] - 1) // 2
-
     return answer
