ea022e42549ae38c95c95b45af43f8b9fb2fdd78
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-09-17 08:04:37 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -2,16 +2,18 @@ from collections import Counter
 
 def solution(weights):
     answer = 0
-    counter = Counter(weights)
-    weight_values = list(counter.keys())
-    G = [2, 3, 4]
-    
-    for i in range(len(weight_values)):
-        for k in range(len(G) - 1):
-            for j in range(k + 1, len(G)):
-                if weight_values[i] * G[k] / G[j] in counter:
-                    print(weight_values[i], weight_values[i] * G[k] // G[j])
-                    answer += counter[weight_values[i]] * counter[weight_values[i] * G[k] // G[j]]
-        answer += counter[weight_values[i]] * (counter[weight_values[i]] - 1) // 2
-    
+    weight_counter = Counter(weights)
+    unique_weights = list(weight_counter.keys())
+    ratios = [2, 3, 4]
+
+    for i in range(len(unique_weights)):
+        for k in range(len(ratios) - 1):
+            for j in range(k + 1, len(ratios)):
+                ratio = unique_weights[i] * ratios[k] / ratios[j]
+                if ratio in weight_counter:
+                    print(unique_weights[i], ratio)
+                    answer += weight_counter[unique_weights[i]] * weight_counter[ratio]
+
+        answer += weight_counter[unique_weights[i]] * (weight_counter[unique_weights[i]] - 1) // 2
+
     return answer
