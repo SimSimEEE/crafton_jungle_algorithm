eecfb6409fb7d873b625c6feb5bf82e5bbad7aaf
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-09-19 06:54:09 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.11 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,41 +1,43 @@
-def dfs(x, y, direction, cost, board, visit, n):
+def dfs(x, y, direction, cost, board, visit, n, dx, dy):
     if board[x][y] or visit[x][y] or x == y == n - 1:
         return
 
     visit[x][y] = True
 
-    directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-    
-    for dx, dy in directions:
-        nx, ny = x + dx, y + dy
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
         if direction == 'x':
             if 0 <= nx < n and board[nx][y] == 0 and not visit[nx][y]:
-                if cost[x][y] + 100 < cost[nx][y]:
-                    cost[nx][y] = cost[x][y] + 100
+                new_cost = cost[x][y] + 100
+                if new_cost < cost[nx][y]:
+                    cost[nx][y] = new_cost
                     visit[nx][y] = False
-                    dfs(nx, y, 'x', cost, board, visit, n)
+                    dfs(nx, y, 'x', cost, board, visit, n, dx, dy)
                     visit[nx][y] = False
 
             if 0 <= ny < n and board[x][ny] == 0 and not visit[x][ny]:
-                if cost[x][y] + 600 < cost[x][ny]:
-                    cost[x][ny] = cost[x][y] + 600
+                new_cost = cost[x][y] + 600
+                if new_cost < cost[x][ny]:
+                    cost[x][ny] = new_cost
                     visit[x][ny] = False
-                    dfs(x, ny, 'y', cost, board, visit, n)
+                    dfs(x, ny, 'y', cost, board, visit, n, dx, dy)
                     visit[x][ny] = False
 
         else:
             if 0 <= ny < n and board[x][ny] == 0 and not visit[x][ny]:
-                if cost[x][y] + 100 < cost[x][ny]:
-                    cost[x][ny] = cost[x][y] + 100
+                new_cost = cost[x][y] + 100
+                if new_cost < cost[x][ny]:
+                    cost[x][ny] = new_cost
                     visit[x][ny] = False
-                    dfs(x, ny, 'y', cost, board, visit, n)
+                    dfs(x, ny, 'y', cost, board, visit, n, dx, dy)
                     visit[x][ny] = False
 
             if 0 <= nx < n and board[nx][y] == 0 and not visit[nx][y]:
-                if cost[x][y] + 600 < cost[nx][y]:
-                    cost[nx][y] = cost[x][y] + 600
+                new_cost = cost[x][y] + 600
+                if new_cost < cost[nx][y]:
+                    cost[nx][y] = new_cost
                     visit[nx][y] = False
-                    dfs(nx, y, 'x', cost, board, visit, n)
+                    dfs(nx, y, 'x', cost, board, visit, n, dx, dy)
                     visit[nx][y] = False
 
 
@@ -52,12 +54,14 @@ def solution(board):
             cost[1][0] = 100
         visit = [[False] * n for _ in range(n)]
 
+        dx, dy = [0, 1, -1, 0], [1, 0, 0, -1]  # Right, Down, Up, Left
+
         if k:
             if not board[0][1]:
-                dfs(0, 1, 'y', cost, board, visit, n)
+                dfs(0, 1, 'y', cost, board, visit, n, dx, dy)
         else:
             if not board[1][0]:
-                dfs(1, 0, 'x', cost, board, visit, n)
+                dfs(1, 0, 'x', cost, board, visit, n, dx, dy)
 
         answer = min(cost[n - 1][n - 1], answer)
         print(*cost, sep='\n', end='\n\n')
