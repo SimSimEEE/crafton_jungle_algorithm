a183411c4082a4848602134b35183cbb6c188f3b
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-09-22 07:28:28 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -1,30 +1,11 @@
 def solution(s):
-    n = len(s)
-    if n <= 1:
-        return n
-
-    dp = [[False] * n for _ in range(n)]
-
-    max_len = 1  
-
-    for i in range(n):
-        dp[i][i] = True
-
-    start = 0 
-
-    for i in range(n - 1):
-        if s[i] == s[i + 1]:
-            dp[i][i + 1] = True
-            max_len = 2
-            start = i
-
-    for length in range(3, n + 1):
-        for i in range(n - length + 1):
-            j = i + length - 1
-            if s[i] == s[j] and dp[i + 1][j - 1]:
-                dp[i][j] = True
-                if length > max_len:
-                    max_len = length
-                    start = i
-
-    return len(s[start:start + max_len])
+    if s == s[::-1] :
+        return len(s)
+    for i in range(1, len(s)) :
+        n = len(s) - i
+        indexList = [x for x in range(len(s) - n + 1)]
+        for e in indexList :
+            p = s[e: e + n]
+            if p == p[::-1] :
+                return len(p)
+    return -1
