df1725f1c74c3b42acdbd1e00cfff9c390a4e7ae
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-09-25 04:09:00 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,6 +1,4 @@
 from datetime import datetime
 
 def solution(date1, date2):
-    dt1 = datetime(date1[0], date1[1], date1[2])
-    dt2 = datetime(date2[0], date2[1], date2[2])
-    return int(dt1 < dt2)
+    return int(date1 < date2)
