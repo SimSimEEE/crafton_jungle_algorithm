b749a77c00019c4459500de93cf205d4efb8b1c8
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-10-06 23:30:24 +0900

    [unrated] Title: 부대복귀, Time: 197.02 ms, Memory: 42.9 MB -BaekjoonHub

--- 프로그래머스/unrated/132266. 부대복귀/부대복귀.py
+++ 프로그래머스/unrated/132266. 부대복귀/부대복귀.py
@@ -11,17 +11,15 @@ def solution(n, roads, sources, destination):
         while queue:
             node, cnt = queue.pop(0)
             for path in graph.get(node, []):
-                if visited[path] is False:
+                if visited[path] == -1:
                     visited[path] = cnt + 1
                     queue.append((path, cnt + 1))
 
-    visited = [False] * (n + 1)
+    visited = [-1] * (n + 1)
     bfs(destination, visited)
     
     answer = []
     for source in sources:
         shortest_path = visited[source]
-        if shortest_path is False:
-            shortest_path = -1
         answer.append(shortest_path)
     return answer
