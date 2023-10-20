2105d9d729dc9766ceb8615fb058b0eb980dd160
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-10-20 10:01:35 +0900

    [Gold IV] Title: 달려달려, Time: 3632 ms, Memory: 229556 KB -BaekjoonHub

--- 백준/Gold/1757. 달려달려/달려달려.py
+++ 백준/Gold/1757. 달려달려/달려달려.py
@@ -5,6 +5,6 @@ for i in range(1, n + 1):
     dp[i][0] = dp[i-1][0]
     for j in range(1, m + 1):
         dp[i][j] = dp[i-1][j - 1] + d[i]
-    for j in range(1, m + 1):
         dp[i][0] = max(dp[i][0], dp[i - j][j])
+    # for j in range(1, m + 1):
 print(dp[-1][0])
