4d9cbdd53fe422d867237a44fc8737b77f85a879
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-10-20 10:00:03 +0900

    [Gold IV] Title: 달려달려, Time: 3676 ms, Memory: 227508 KB -BaekjoonHub

---
+++ 백준/Gold/1757. 달려달려/달려달려.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+d = [0] + list(int(input()) for _ in range(n))
+dp = [[0] * (m + 1) for _ in range(n+1)]
+for i in range(1, n + 1):
+    dp[i][0] = dp[i-1][0]
+    for j in range(1, m + 1):
+        dp[i][j] = dp[i-1][j - 1] + d[i]
+    for j in range(1, m + 1):
+        dp[i][0] = max(dp[i][0], dp[i - j][j])
+print(dp[-1][0])
