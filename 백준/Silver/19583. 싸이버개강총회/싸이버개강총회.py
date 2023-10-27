283d5d8b46156e04f334bc7ced6218fb2089d0f4
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-10-27 15:21:41 +0900

    [Silver II] Title: 싸이버개강총회, Time: 260 ms, Memory: 130380 KB -BaekjoonHub

---
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.py
@@ -0,0 +1,23 @@
+def time_to_min(time_str):
+    hours, minutes = map(int, time_str.split(':'))
+    return hours * 60 + minutes
+
+s, e, q = map(time_to_min, input().split())
+streaming = {}
+answer = 0
+
+while True:
+    try:
+        t, n = input().split()
+        t = time_to_min(t)
+    except:
+        break
+
+    if n not in streaming:
+        if t <= s:
+            streaming[n] = 1
+    elif e <= t <= q:
+        del streaming[n]
+        answer += 1
+
+print(answer)
