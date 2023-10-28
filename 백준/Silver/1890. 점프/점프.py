06c4ca772bf665dadeebd53c613ee033f3d7401f
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-10-28 18:59:53 +0900

    [Silver I] Title: 점프, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.py
+++ 백준/Silver/1890. 점프/점프.py
@@ -5,9 +5,9 @@ dp[0][0] = 1
 for i in range(n):
     for j in range(n):
         if i == n - 1 and j == n - 1:
+            print(dp[-1][-1])
             break
         if i + board[i][j] < n:
             dp[i + board[i][j]][j] += dp[i][j]
         if j + board[i][j] < n:
             dp[i][j + board[i][j]] += dp[i][j]
-print(dp[-1][-1])
