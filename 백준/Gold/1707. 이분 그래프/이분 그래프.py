361a693649df63d24a9bdd7c8fe3a85fae8ab30f
SimSimEEE <103261543+SimSimEEE@users.noreply.github.com>
2023-11-07 01:07:28 +0900

    [Gold IV] Title: 이분 그래프, Time: 984 ms, Memory: 53624 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,31 +1,35 @@
 import sys
 sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
 
-def DFS(V,color):
+def is_bipartite(V, tree, visited, color):
     visited[V] = color
-    for i in tree[V]:
-        if visited[i] == 0:
-            if not DFS(i,-color):
+    for neighbor in tree[V]:
+        if visited[neighbor] == 0:
+            if not is_bipartite(neighbor, tree, visited, -color):
                 return False
-        elif visited[i] == color:
+        elif visited[neighbor] == color:
             return False
     return True
 
-for _ in range(int(input())):
-    V, E = map(int, input().split())
-    tree = [[]for _ in range(V+1)]
-    visited = [0]*(V+1)
-    flag = 0
-    for _ in range(E):
-        u, v = map(int, input().split())
-        tree[u].append(v)
-        tree[v].append(u)
+def check_bipartite_graph(test_cases):
+    for _ in range(test_cases):
+        V, E = map(int, input().split())
+        tree = [[] for _ in range(V + 1)]
+        visited = [0] * (V + 1)
+        for _ in range(E):
+            u, v = map(int, input().split())
+            tree[u].append(v)
+            tree[v].append(u)
+        flag = 0
+        for i in range(V + 1):
+            if visited[i] == 0:
+                if not is_bipartite(i, tree, visited, 1):
+                    print("NO")
+                    break
+        else:
+            print("YES")
 
-    for i in range(V+1):
-        if visited[i] == 0:
-            if not DFS(i,1):
-                print("NO")
-                break
-    else:
-        print("YES")
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    test_cases = int(input())
+    check_bipartite_graph(test_cases)
